f614b008c916ef95e643cb724d2585a7a73871de
issalo1998 <issalo163@gmail.com>
2024-12-18 20:21:07 +0000

    test gitignore

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 < ?php
  // Fonction principale pour afficher le message
 function main() {
- echo "Hello, GitHub! Je suis sur la preprod";
+ echo "Hello, GitHub! Je suis sur la  !!!";
 }
 // Appel de la fonction principale
 main();
